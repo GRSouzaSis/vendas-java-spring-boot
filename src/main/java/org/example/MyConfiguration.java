46cdffb86a9ff54b34fdc71d188b3539bbf03e14
gisouza <givaribeiro23@gmail.com>
2023-09-21 17:29:34 -0300

    Ambientes dev - prod

--- src/main/java/org/example/MyConfiguration.java
+++ src/main/java/org/example/MyConfiguration.java
@@ -1,6 +1,9 @@
 package org.example;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
 @Configuration
+@Profile("development")
 public class MyConfiguration {
 
 }
