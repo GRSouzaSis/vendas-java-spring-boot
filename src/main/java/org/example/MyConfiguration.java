a1c16e4334a886dd5a5427c7bdac7a4a1d1f3d9d
gisouza <givaribeiro23@gmail.com>
2023-09-21 17:38:45 -0300

    Development configuration

--- src/main/java/org/example/MyConfiguration.java
+++ src/main/java/org/example/MyConfiguration.java
@@ -2,8 +2,7 @@ package org.example;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
-@Configuration
-@Profile("development")
+@Development
 public class MyConfiguration {
 
 }
